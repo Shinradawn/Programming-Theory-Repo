bd01dd86174f4a7a8ef5c2a2da506683cc0d5aa8
Shinradawn <99540580+Shinradawn@users.noreply.github.com>
2022-03-23 10:44:13 +0000

    Finished

--- Assets/Scripts/DisplayUsername.cs
+++ Assets/Scripts/DisplayUsername.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DisplayUsername : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI welcomeText;
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.buildIndex == 1)
+        {
+            welcomeText.text = "Hi " + GameManager.Instance.Name +". As you can see. All the shapes behave in different ways. Click on them too.";
+
+        }
+
     }
 
     // Update is called once per frame
